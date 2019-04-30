be02592258ef4aa35140ead8e55376faf528e69b
sergii <sergii@antvoice.com>
2019-04-30 17:17:29 +0200

    upd

--- reproduce.sh
+++ reproduce.sh
@@ -1,2 +1,7 @@
 dotnet build -c Release full.sln
+
+#this works :
+dotnet build -c Release --no-dependencies project2/project2.csproj
+
+#this fails :
 dotnet build -c Release --no-dependencies partial.sln
